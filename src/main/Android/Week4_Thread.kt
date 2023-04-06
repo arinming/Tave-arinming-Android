3b01402126a3e50c9f7b954ff246f8200dcf5c6e
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-06 18:43:00 +0900

    ERROR : SDK

--- src/main/Android/Week4_Thread.kt
+++ src/main/Android/Week4_Thread.kt
@@ -1,20 +1,3 @@
-package com.example.studyproject
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import sun.rmi.runtime.Log
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
-
-
-
-
-
 // 안드로이드 시스템은 메모리 이외 다른 곳에서 데이터를 가져오는 작업을
 // 백그라운드 스레드에서 처리하도록 권장한다
 
@@ -28,8 +11,6 @@ class WorkerThread: Thread() {
         var i = 0
         while (i < 10) {
             i += 1
-            Log.i("WorkerThread", "$i")
         }
     }
-
 }
