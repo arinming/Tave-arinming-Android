566ef25d5d9ba51ef517357d186c4ddafaef7257
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-09 00:04:12 +0900

    코틀린 기초: 변수와 자료형

--- Android_Study_Project/app/src/main/java/com/example/studyproject/MainActivity.kt
+++ Android_Study_Project/app/src/main/java/com/example/studyproject/MainActivity.kt
@@ -9,32 +9,33 @@ class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"    // 변하지 않는 값은 val로 선언
 
    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       super.onCreate(savedInstanceState)
+       setContentView(R.layout.activity_main)
 
-        var myName = "김아린"
-        Log.d(TAG, "my name = $myName")
-        myName = "아린"   // var는 선언한 뒤 값을 변경할 수 있다
-        Log.d(TAG, "my name = $myName")
+       var myName = "김아린"
+       Log.d(TAG, "my name = $myName")
+       myName = "아린"   // var는 선언한 뒤 값을 변경할 수 있다
+       Log.d(TAG, "my name = $myName")
 
-        val PI = 3.141592   // val은 선언한 뒤 값을 변경할 수 없다
-        // 변하지 않는 상수를 정의할 때 사용
+       val PI = 3.141592   // val은 선언한 뒤 값을 변경할 수 없다
+       // 변하지 않는 상수를 정의할 때 사용
        Log.d(TAG, "pi= $PI")
 
 
        var myNumber = "1,2,3,4,5,6"
        var thisWeekNumbers = "5,6,7,8,9,10"
-       if(myNumber == thisWeekNumbers) {
+       if (myNumber == thisWeekNumbers) {
            Log.d(TAG, "당첨되었습니다")
        } else {
            Log.d(TAG, "당첨되지 않았습니다")
        }
 
-       for(index in 1..10) {    // 1..10 => 1부터 10까지 반복, 키워드를 변수명으로 쓰지 말기
+       for (index in 1..10) {    // 1..10 => 1부터 10까지 반복, 키워드를 변수명으로 쓰지 말기
            Log.d(TAG, "현재 숫자는 ${index}입니다") // 문자열 표시에 $ 표시 쓸 때는 한 칸 띄어쓰기
-            // $ 표시를 띄어쓰지 않고 표기하려면 변수명에 {}
+           // $ 표시를 띄어쓰지 않고 표기하려면 변수명에 {}
        }
-    }
+
+   }
 }
 
 
---
+++ Study/KotlinStudy.kt
@@ -0,0 +1,39 @@
+fun main(args: Array<String>) {
+    var a: Int = 123
+    // 코틀린은 Null 값을 허용하지 않기 때문에 초기화를 꼭 해야한다
+    println(a)
+
+
+
+    // 코틀린의 자료형
+    var intValue: Int = 1234
+    var LongValue: Long = 1234L
+    var intValueByHex: Int = 0x1af  // 16진수
+    var intValueByBin: Int = 0b10110110 // 2진수
+    // 코틀린은 8진수의 표기 지원 X
+    // 실수의 기본은 Double형이며 소수점을 포함하여 쓰기
+    var doubleValue: Double = 123.5
+    var doubleValueWithExp: Double = 123.5e10   // 필요시 지수 표기법을 추가
+    var floatValue: Float = 123.5f  // 16비트의 Float 표기법
+
+    // 코틀린의 문자열은 글자 하나하나가 2bytes의 메모리 공간을 사용
+    var charValue: Char = 'a'   // 작은 따옴표로 감싸서 표기
+
+    // Boolean 값
+    var booleanValue: Boolean = true
+
+    // 문자열
+    val stringValue = "탭 \t"
+    val multiLineStringValue = """
+        특수문자 /(!@*($* 그대로 사용 가능
+        \r 첫 열로 커서 옮김
+        \n 개행
+        \n 백스페이스
+        \' 작은 따옴표
+        \" 큰 따옴표
+        \\ 역슬래스
+        \$
+        \uxxxx 유니코드 문자
+    """.trimIndent()
+}
+
