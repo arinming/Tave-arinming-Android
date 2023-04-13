50b8b6c5da6e615ce1f3cb4b592c45a095e44358
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-13 17:14:12 +0900

    스톱워치 : 스톱워치 시작 로직

--- Tesk1/app/src/main/java/com/example/tesk1_timer/MainActivity.kt
+++ Tesk1/app/src/main/java/com/example/tesk1_timer/MainActivity.kt
@@ -30,6 +30,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun start() {
         binding.btnStart.text = "일시정지"
+        isRunning = true    // 실행상태 변경
 
+        // 스톱워치를 시작하는 로직
+        timer = timer(period = 10) {
+            time++  // 10밀리초 단위 타이머
+
+            // 시간 계산
+            val milli_second = time % 100
+            val second = (time % 6000) / 100
+            val minute = time / 6000
+
+            // 밀리초
+            binding.tvTimeMil.text = if (milli_second < 10) ".0${milli_second}"
+            else ".${milli_second}"
+
+            // 초
+            binding.tvTimeSec.text = if (second < 10) ":0${second}"
+            else ":${second}"
+
+            // 분
+            binding.tvTimeMin.text = "${minute}"
+
+        }
     }
 }
